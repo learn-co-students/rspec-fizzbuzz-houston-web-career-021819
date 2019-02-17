92cd0ac5238ea7583f611785b4d68912a908f842
JingChen <jingchenjc2019@gmail.com>
2019-02-17 04:00:56 -0600

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if (int % 5 == 0) && (int % 3 == 0)
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  else
+    nil
+  end
+end
+#fizzbuzz(3) # => You should see a return of "Fizz"
+#fizzbuzz(5) # => You should see a return of nil
+#fizzbuzz()  # => You should get an ArgumentError
